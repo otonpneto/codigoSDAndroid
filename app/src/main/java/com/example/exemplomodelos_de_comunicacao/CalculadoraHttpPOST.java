f20fc41dc7cc3df185d23d01f01083c8bea2b302
otonp <otonpneto@gmail.com>
2021-06-30 22:23:25 -0300

    Comentando clientes socket e HTTP.

--- app/src/main/java/com/example/exemplomodelos_de_comunicacao/CalculadoraHttpPOST.java
+++ app/src/main/java/com/example/exemplomodelos_de_comunicacao/CalculadoraHttpPOST.java
@@ -17,12 +17,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
-
+/**
+ * Cliente HTTP para exemplificar o modelo requisição-resposta
+ * Estende uma AsyncTask, responsável por realizar uma tarefa em segundo plano, cujo resultado é publicado na thread da interface
+ * Tal artíficio é utilizado para que a interface não fique bloqueada enquanto a operação é realizada
+ */
 public class CalculadoraHttpPOST extends AsyncTask<Void, Void, String> {
-
+    //Cria os objetos necessários
+    //tv é utilizado para exibir texto na tela do app
     TextView tv;
+    //Strings representando os dois operadores e a operação da calculadora (1-soma;2-subtração;3-multiplicação;4-divisão)
     String oper1,oper2,op;
+    //Objeto responsável por instanciar e executar a CalculadoraHttpPOST
     PrecisaCalcular pc;
+    //Construtores tanto para o caso de instanciar o objeto com uma TextView, quanto para o caso de instanciar com um objeto PrecisaCalcular
+    //Ao usar a TextView, no próprio onPostExecute escreve o resultado da operação na tela do usuário
+    //Ao usar o objeto PrecisaCalcular, passa para ele o resultado e ele que ficará responsável por imprimí-lo na tela do usuário
     public CalculadoraHttpPOST(TextView tv, String oper1, String oper2, String op){
         this.tv=tv;
         this.oper1=oper1;
@@ -38,62 +48,88 @@ public class CalculadoraHttpPOST extends AsyncTask<Void, Void, String> {
         this.op=op;
 
     }
+
+    //Ação propriamente dita que será realizada assincronamente, não travando a tela do usuário enquanto executa
+    //Seu retorno é passado por parâmetro para o método onPostExecute
     @Override
     protected String doInBackground(Void... voids) {
         String result="";
         try {
-
+            //Cria um objeto URL a partir de uma string (endereço do servidor PHP)
            URL url = new URL("https://double-nirvana-273602.appspot.com/?hl=pt-BR");
+            //Cria uma instância de uma conexão que representa o objeto remoto referenciado pela URL
            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            //Seta o máximo de tempo (em ms) de espera pela resposta do servidor, após isso lança SocketTimeoutException
             conn.setReadTimeout(10000);
+            //Seta o máximo de tempo (em ms) de espera pelo estabelecimento da conexão com o servidor, após isso lança SocketTimeoutException
             conn.setConnectTimeout(15000);
+            //Seta o método para POST
             conn.setRequestMethod("POST");
+            //Seta que a conexão será usada tanto para input quanto output
             conn.setDoInput(true);
             conn.setDoOutput(true) ;
 
             //ENVIO DOS PARAMETROS
+            //Cria um objeto OutputStream para a conexão
             OutputStream os = conn.getOutputStream();
+            //Cria um objeto BufferedWriter a partir do output da conexão para escrever os parâmetros da requisição
+            //Criado passando o outputstream da conexão e o charset suportado
             BufferedWriter writer = new BufferedWriter(
                     new OutputStreamWriter(os, "UTF-8"));
+            //Escreve os parâmetros no objeto writer. No caso, o operador 1, o operador 2 e a operação da requisição, conforme implementado e esperado pelo servidor remoto PHP
             writer.write("oper1="+oper1+"&oper2="+oper2+"&operacao="+op);
+            //Limpa o objeto writer para depois fechá-lo
             writer.flush();
             writer.close();
+            //Fecha o outputstream da conexão
             os.close();
-
+            //Retorna o status de resposta da requisição
             int responseCode=conn.getResponseCode();
+            //Se a resposta da requisição foi sucesso
             if (responseCode == HttpsURLConnection.HTTP_OK) {
 
-                //RECBIMENTO DOS PARAMETROS
+                //RECEBIMENTO DOS PARAMETROS
 
-
+                //Cria um objeto para ler a resposta dada pelo servidor, a partir do inputstream da conexão e indicando o charset suportado
                 BufferedReader br = new BufferedReader(
                         new InputStreamReader(conn.getInputStream(), "utf-8"));
                 StringBuilder response = new StringBuilder();
                 String responseLine = null;
+                //Lê resposta e adiciona no objeto response
+                //O método readLine() lê uma linha até encontrar o caractere '\n'
+                //Quando chegar ao final e a linha estiver nula, já foram lidas todas as linhas da resposta
                 while ((responseLine = br.readLine()) != null) {
                     response.append(responseLine.trim());
                 }
+                //Converte o valor do StringBuilder em uma string para ser retornada
                 result = response.toString();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
+        //Retorna o resultado enviado pelo servidor que será recebido como parâmetro no método onPostExecute
         return result;
         //Codigo
     }
 
-
+    /* Este método é executado antes da Thread ser iniciada.
+     * Não é obrigatório
+     * Executa na mesma thread da interface gráfica
+     */
     @Override
     protected void onPreExecute() {
         //Codigo
     }
 
-
+    /* Este método é quem recebe o retorno do doInBackground
+     * Executa na mesma thread da interface gráfica
+     */
     @Override
     protected void onPostExecute(String result) {
-        //Codigo
+        /* Se o TextView não for nulo, seta-o com o resultado
+         * Caso contrário, chama o método do objeto PrecisaCalcular, que foi quem chamou essa classe, a fim de passar o resultado da operação, para que ela imprima o resultado na tela
+         */
         if(this.tv!=null) {
             this.tv.setText(result);
         }else {
--- app/src/main/java/com/example/exemplomodelos_de_comunicacao/CalculadoraSocket.java
+++ app/src/main/java/com/example/exemplomodelos_de_comunicacao/CalculadoraSocket.java
@@ -9,12 +9,22 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-
+/**
+ * Cliente para o serviço de calculadora disponibilizado via sockets.
+ * Estende uma AsyncTask, responsável por realizar uma tarefa em segundo plano, cujo resultado é publicado na thread da interface
+ * Tal artíficio é utilizado para que a interface não fique bloqueada, enquanto a operação é realizada
+ */
 public class CalculadoraSocket extends AsyncTask<Void, Void, String> {
-
+    //Cria os objetos necessários
+    //tv é utilizado para exibir texto na tela do app
     TextView tv;
+    //Strings representando os dois operadores e a operação da calculadora (1-soma;2-subtração;3-multiplicação;4-divisão)
     String oper1,oper2,op;
+    //Objeto responsável por instanciar e executar a CalculadoraSocket
     PrecisaCalcular pc;
+    //Construtores tanto para o caso de instanciar o objeto com uma TextView, quanto para o caso de instanciar com um objeto PrecisaCalcular
+    //Ao usar a TextView, no próprio onPostExecute escreve o resultado da operação na tela do usuário
+    //Ao usar o objeto PrecisaCalcular, passa para ele o resultado e ele que ficará responsável por imprimí-lo na tela do usuário
     public CalculadoraSocket(TextView tv, String oper1, String oper2, String op){
         this.tv=tv;
         this.oper1=oper1;
@@ -30,6 +40,8 @@ public class CalculadoraSocket extends AsyncTask<Void, Void, String> {
         this.op=op;
 
     }
+    //Ação propriamente dita que será realizada assincronamente, não travando a tela do usuário enquanto executa
+    //Seu retorno é passado por parâmetro para o método onPostExecute
     @Override
     protected String doInBackground(Void... voids) {
         String result="";
@@ -37,42 +49,71 @@ public class CalculadoraSocket extends AsyncTask<Void, Void, String> {
         try {
 
             //Conexão com o Servidor
+            //Vamos criar uma conexão via socket na porta 9090 do servidor que está rodando o CalculadoraServerSocket.java
             Socket clientSocket = new Socket("192.168.1.65", 9090);
+            /*
+             * O envio dos dados para o servidor será feito através de um outputStream do
+             * socket. Para isso, será utilizada uma instância da classe concreta
+             * DataOutputStream
+             */
             DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
 
             //Enviando os dados
+            /*
+             * A stream enviada será composta por operador, operando 1 e operando 2, nesta
+             * ordem. Após cada um deles, será incluída uma quebra de linha ("\n"), afim de
+             * delimitar os dados.
+             */
             socketSaidaServer.writeBytes(op+"\n");
             socketSaidaServer.writeBytes(oper1+ "\n");
             socketSaidaServer.writeBytes( oper2+ "\n");
+            /*
+             * descarrega o buffer na stream, se ainda houver, que será então encaminhado ao
+             * servidor.
+             */
             socketSaidaServer.flush();
 
             //Recebendo a resposta
+            /*
+             * O recebimento do resultado do servidor será feito através de um InputStream
+             * do socket. A stream recebida é formada pelo resultado seguido de uma quebra
+             * de linha. Como o resultado encontra-se delimitado por quebra de linha ("\n"),
+             * é conveniente instanciar um BufferedReader a partir da InputStream do socket
+             * para, logo em seguida, utilizar o método readLine para obtê-lo.
+             */
             BufferedReader messageFromServer = new BufferedReader
                     (new InputStreamReader(clientSocket.getInputStream()));
             result=messageFromServer.readLine();
 
-          //  System.out.println("resultado="+result);
+            // Fecha o socket.
             clientSocket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
+        //Retorna o resultado enviado pelo servidor que será recebido como parâmetro no método onPostExecute
         return result;
         //Codigo
     }
 
-
+        /* Este método é executado antes da Thread ser iniciada.
+         * Não é obrigatório
+         * Executa na mesma thread da interface gráfica
+         */
     @Override
 protected void onPreExecute() {
         //Codigo
         }
 
-
+    /* Este método é quem recebe o retorno do doInBackground
+     * Executa na mesma thread da interface gráfica
+     */
 @Override
 protected void onPostExecute(String result) {
-        //Codigo
+    /* Se o TextView não for nulo, seta-o com o resultado
+     * Caso contrário, chama o método do objeto PrecisaCalcular, que foi quem chamou essa classe, a fim de passar o resultado da operação, para que ela imprima o resultado na tela
+     */
            if(this.tv!=null) {
                this.tv.setText(result);
            }else {
